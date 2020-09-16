3bdcb9115fdf47a13d9e38e65d32fec47f58b58a
Sanjay Narvekar <Sanjay.Narvekar@gmail.com>
2020-09-16 15:38:38 -0500

    Updated fss terraform file

--- fss.tf
+++ fss.tf
@@ -8,3 +8,13 @@ resource "oci_file_storage_file_system" "shared_file_system" {
 
     display_name = var.file_system_display_name
 }
+
+resource "oci_file_storage_mount_target" "shared-mount-target" {
+  #Required
+  availability_domain = data.oci_identity_availability_domain.ad.name
+  compartment_id      = var.compartment_ocid
+  subnet_id           = oci_core_subnet.subnetf.id
+
+  #Optional
+  display_name = "Shared File Storage"
+}
